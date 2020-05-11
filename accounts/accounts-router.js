83153e529f8b0dc09972f9eb1211c33b61b20df7
Harrison <hmseaborn@gmail.com>
2020-05-11 12:42:59 -0700

    added some global mids
    get accounts and get by id almost functional.. console log stage of by id

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -1,6 +1,39 @@
-const express = require("express");
-const knex = require("../data/dbConfig");
+const express = require('express')
+const knex = require('../data/dbConfig')
+const router = express.Router()
 
-const router = express.Router();
+router.get('/', (req, res) => {
+  knex
+    .select('*')
+    .from('accounts')
+    .then((accounts) => {
+      if (accounts) {
+        res.status(200).json(accounts)
+      } else {
+        res
+          .status(404)
+          .json({ message: 'there are no accounts in the database' })
+      }
+    })
+    .catch((error) => {
+      console.log(error.message)
+      res.status(500).json(error.message)
+    })
+})
 
-module.exports = router;
+router.get('/:id', (req, res) => {
+  const { id } = req.params
+  knex
+    .select('*')
+    .from('accounts')
+    .where({ id })
+    .then((account) => {
+      console.log(account)
+    })
+    .catch((error) => {
+      console.log(error.message)
+      res.status(500).json(error.message)
+    })
+})
+
+module.exports = router
